8da63f2e08e56211ca7a9fcc3b4f9ce3a1e08cde
NightDevie <nightdevie@gmail.com>
2020-01-28 13:47:43 +0000

    Made Unit Prefabs

--- NightProject/Assets/Scripts/InstantiateUnit.cs
+++ NightProject/Assets/Scripts/InstantiateUnit.cs
@@ -8,10 +8,11 @@ public class InstantiateUnit : MonoBehaviour
     public UnitsInventory unitsInventory;
     public GameObject unitPrefab;
     public GameObject parentObject;
-    public Button button;
+    private Button button;
 
     private void Start()
     {
+        button = GetComponent<Button>();
         button.onClick.AddListener(() => InstantiatePrefab(1));
     }
 
@@ -22,5 +23,6 @@ public class InstantiateUnit : MonoBehaviour
 
         Unit unit = instantiatedPrefab.gameObject.GetComponent<Unit>();
         instantiatedPrefab.name = "[" + index + "]" + unit.unitName;
+        Debug.Log("Instantiated");
     }
 }
